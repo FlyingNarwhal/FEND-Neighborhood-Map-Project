021e2ca478e0d5cde7d84b2909f1d281ab707304
Joe Dahle <joe.dahle@gmail.com>
2016-02-05 10:45:27 -0700

    fix: fix error handling for google maps API

---
+++ app/js/error.js
@@ -0,0 +1,7 @@
+var googleError = function() {
+	$("error").css('block');
+	$(".location").css("display", "none");
+	$("#map").css("background-color", "#e1e1e1");
+	$(".yelp-info").css("display", "none");
+	$(".api-error").css("display", "none");
+};
