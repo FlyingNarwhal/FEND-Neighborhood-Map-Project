9e7e4dff7e06ce29771f21bf160fa3cfa2879025
Joe Dahle <joe.dahle@gmail.com>
2016-02-04 18:41:17 -0700

    fix: infoView display issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,5 @@
 var gulp = require('gulp');
-var uglify = require('gulp-uglify');
 var ghPages = require('gulp-gh-pages');
-
-gulp.task('default', function() {
-  // place code for your default task here
-});
-
-gulp.task('compress', function() {
-	return gulp.src('app/*.js')
-	.pipe(uglify())
-	.pipe(gulp.dest('dist/'));
-});
 
 gulp.task('deploy', function() {
 	return gulp.src('./app/**/*')
